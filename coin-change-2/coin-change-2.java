8bc9d6bbcc5a6f0f9434b56b87349ddf1241815c
Anand Mehrotra <62706186+anandmehrotraTR@users.noreply.github.com>
2023-10-26 19:19:19 -0500

    Time: 4 ms, Memory: 39.7 MB - LeetHub

--- coin-change-2/coin-change-2.java
+++ coin-change-2/coin-change-2.java
@@ -1,20 +1,23 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
+        int[] dp = new int[amount + 1];
+        //2D to 1D dp-> C relies on c + 1 only 
+        // and the amount is the only factor that varies form o to amount
         
-        for(int i = 0; i < coins.length; i++) {
-            dp[i][0] = 1;
-        }
+        dp[0] = 1; 
         
         for(int c = coins.length - 1; c >= 0; c--) {
             for (int a = 1; a <= amount; a++) {
                 if (coins[c] > a ) {
-                    dp[c][a] = dp[c+1][a];
+                    //don't pick the current coin, just continue
+                   // dp[c][a] = dp[c+1][a];
+                    continue;
                 } else {
-                    dp[c][a] = dp[c][a - coins[c]] + dp[c + 1][a];
+                    //pick the coin, don't pick is already satisfied by doing a + on dp[a]
+                    dp[a] += dp[a - coins[c]];
                 }
             }
         }
-        return dp[0][amount];
+        return dp[amount];
     }
 }
